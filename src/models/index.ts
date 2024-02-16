c322c41aedd783ed7c338c82934512f737cdc807
diegoleondev <diegoleon.dev@gmail.com>
2024-02-15 22:32:13 -0600

    model errors handler created

---
+++ src/models/index.ts
@@ -0,0 +1,29 @@
+import type { Details } from "validators/response.js";
+
+export class ModelError<T extends Details> extends Error {
+  public details: Details;
+
+  constructor(details: T) {
+    super("ModelError");
+    this.details = details;
+  }
+
+  public addDetails(details: Details) {
+    this.details = {
+      ...this.details,
+      ...details,
+    };
+  }
+}
+
+export const modelErrorHandlerForResponse = async <T>(
+  promise: Promise<T>,
+  callbackError: (details: Details) => Error,
+) => {
+  return await promise.catch((e) => {
+    if (e instanceof ModelError) {
+      throw callbackError(e.details);
+    }
+    throw e;
+  });
+};
