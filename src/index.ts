d44bdedb556274d45aaf1b25f62a2b00e123ab5f
diegoleondev <diegoleon.dev@gmail.com>
2024-02-19 13:26:40 -0600

    config https

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
+import https = require("node:https");
+import fs = require("node:fs");
+
 import app from "./app.js";
+import { ENV, MODES } from "./constants/index.js";
 import { connectDatabase } from "./database/database.js";
+
+const sslCredentials = {
+  key: fs.readFileSync(ENV.SSL_KEY_PATH),
+  cert: fs.readFileSync(ENV.SSL_CERT_PATH),
+  ca:
+    ENV.SERVER_MODE === MODES.PRODUCTION
+      ? fs.readFileSync(ENV.SSL_CA_PATH)
+      : undefined,
+};
+
+const server = https.createServer(sslCredentials, app);
 
 (async () => {
   await connectDatabase();
   console.log("[*] Database connected");
 
-  app.listen(app.get("port"), () => {
+  server.listen(app.get("port"), () => {
     console.log(`[*] Server running on port ${app.get("port")}`);
   });
 })().catch((error) => {
