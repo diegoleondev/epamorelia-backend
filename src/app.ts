12852963778c5d1985b21b5249dd8e6ed67e1d37
diegoleondev <diegoleon.dev@gmail.com>
2024-03-06 11:30:08 -0600

    added configs

--- src/app.ts
+++ src/app.ts
@@ -7,9 +7,13 @@ import morgan from "morgan";
 
 import { ENV, STATUS_CODE } from "./constants/index.js";
 import response from "./lib/response/response.js";
-import cors from "./middlewares/cors.js";
+// TODO: Configure CORS Package
+// import cors from "./middlewares/cors.js";
+import cors from "cors";
 import authRouter from "./routes/auth.js";
-import invitationRouter from "./routes/invitation.js";
+import branchRouter from "./routes/branch.js";
+import init from "./routes/init.js";
+import userInvitationRouter from "./routes/user-invitation.js";
 
 const app = express();
 const { SERVER_PORT } = ENV;
@@ -17,7 +21,7 @@ const { SERVER_PORT } = ENV;
 app.set("port", SERVER_PORT);
 
 app.use(morgan("dev"));
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use(cookiesParser());
 app.use(express.static("public"));
@@ -27,17 +31,19 @@ app.get("/", (req, res) => {
 });
 
 app.use(authRouter);
-app.use(invitationRouter);
+app.use(userInvitationRouter);
+app.use(branchRouter);
+app.use(init);
 
 // Error handler
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+
   try {
     const error = err as ResponseError;
-    console.error("[A] HANDLED", err.details);
     response.error(res, error.details ?? {}, error.statusCode);
   } catch (error) {
     response.error(res, {}, STATUS_CODE.UNKNOWN_ERROR);
-    console.error("[B] UNKNOWN", error);
   }
 });
 
