11008d9e9d87d874dc9e53538ee9e1ff448b773c
diegoleondev <diegoleon.dev@gmail.com>
2024-03-29 08:26:09 -0600

    [feature] added optional auth

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -11,42 +11,69 @@ import { verifyAuthUser } from "../utils/auth/token.js";
 import * as UserTokens from "../models/user-tokens.js";
 import { findByPkUserModel } from "../models/user.js";
 
-const auth = (async (req: Request, res: Response, next: NextFunction) => {
-  const token = (req.header(HEADERS.AUTH_TOKEN) ??
-    req.cookies.accessToken ??
-    req.header("Authorization")?.split(" ")[1]) as string | undefined;
+interface AuthOptions {
+  required?: boolean;
+}
 
-  if (token === undefined)
-    return response.error(res, {}, STATUS_CODE.UNAUTHORIZED);
+interface UnauthorizedOptions {
+  req: Request;
+  res: Response;
+  next: NextFunction;
+  required: boolean;
+}
 
-  const payload = verifyAuthUser(token);
+const unauthorizedMiddleware = (options: UnauthorizedOptions) => {
+  const { required, res, req, next } = options;
+  return () => {
+    if (required) {
+      return response.error(res, {}, STATUS_CODE.UNAUTHORIZED);
+    }
 
-  if (payload === undefined)
-    return response.error(res, {}, STATUS_CODE.UNAUTHORIZED);
-
-  const isValid = await UserTokens.checkToken({
-    token,
-    userId: payload.userId,
-  }).catch(() => false);
-
-  if (!isValid) {
-    return response.error(res, {}, STATUS_CODE.UNAUTHORIZED);
-  }
-
-  const userResult = await findByPkUserModel({ id: payload.userId });
-
-  if (!userResult.success || userResult.data === null) {
-    return response.error(res, {}, STATUS_CODE.UNAUTHORIZED);
-  }
-
-  const { data } = userResult;
-
-  req.user = {
-    role: ROLES[data.roleId as keyof typeof ROLES],
-    ...data,
+    req.authenticated = false;
+    next();
   };
+};
 
-  next();
-}) as RequestHandler<unknown, unknown, unknown, unknown>;
+const auth = (options?: AuthOptions) =>
+  (async (req: Request, res: Response, next: NextFunction) => {
+    const { required = true } = options ?? {};
+
+    const token = (req.header(HEADERS.AUTH_TOKEN) ??
+      req.cookies.accessToken ??
+      req.header("Authorization")?.split(" ")[1]) as string | undefined;
+
+    const unauthorized = unauthorizedMiddleware({ res, req, next, required });
+
+    if (token === undefined) return unauthorized();
+
+    const payload = verifyAuthUser(token);
+
+    if (payload === undefined) return unauthorized();
+
+    const isValid = await UserTokens.checkToken({
+      token,
+      userId: payload.userId,
+    }).catch(() => false);
+
+    if (!isValid) {
+      return unauthorized();
+    }
+
+    const userResult = await findByPkUserModel({ id: payload.userId });
+
+    if (!userResult.success || userResult.data === null) {
+      return unauthorized();
+    }
+
+    const { data } = userResult;
+
+    req.authenticated = true;
+    req.user = {
+      role: ROLES[data.roleId as keyof typeof ROLES],
+      ...data,
+    };
+
+    next();
+  }) as RequestHandler<unknown, unknown, unknown, unknown>;
 
 export default auth;
