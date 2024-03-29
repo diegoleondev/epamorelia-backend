c4a1952fffc38745c978a990da3dbbf70cce5f6f
diegoleondev <diegoleon.dev@gmail.com>
2024-03-29 10:48:36 -0600

    created form routes

---
+++ src/routes/form.ts
@@ -0,0 +1,62 @@
+import { Router } from "express";
+import {
+  createFormUserDataController,
+  getAllFormUserDataController,
+  getFormUserDataController,
+  updateFormUserDataController,
+  updateFormUserDataPublicController,
+} from "../controllers/form-user-data.js";
+import auth from "../middlewares/auth.js";
+import checkPayload from "../middlewares/check-payload.js";
+import {
+  createFormUserDataValidator,
+  deleteFormUserDataValidator,
+  getAllFormUserDataValidator,
+  getFormUserDataValidator,
+  updateFormUserDataPublicValidator,
+  updateFormUserDataValidator,
+} from "../validators/form-user-data.js";
+
+const formRouter = Router();
+
+formRouter.get(
+  "/form/user-data/:id",
+  checkPayload(getFormUserDataValidator),
+  getFormUserDataController,
+);
+
+formRouter.get(
+  "/form/user-data",
+  auth(),
+  checkPayload(getAllFormUserDataValidator),
+  getAllFormUserDataController,
+);
+
+formRouter.post(
+  "/form/user-data",
+  auth(),
+  checkPayload(createFormUserDataValidator),
+  createFormUserDataController,
+);
+
+formRouter.patch(
+  "/form/user-data/:id",
+  auth(),
+  checkPayload(updateFormUserDataValidator),
+  updateFormUserDataController,
+);
+
+formRouter.patch(
+  "/form/user-data/:id/public/",
+  checkPayload(updateFormUserDataPublicValidator),
+  updateFormUserDataPublicController,
+);
+
+formRouter.delete(
+  "/form/user-data/:id",
+  auth(),
+  checkPayload(deleteFormUserDataValidator),
+  getFormUserDataController,
+);
+
+export default formRouter;
